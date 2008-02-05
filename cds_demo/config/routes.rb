c6391c337ba7c8ce7b165916e9d6ce75d45db4f4
timcharper <timcharper@4972a71b-da31-0410-b7dd-373b726a5e75>
2008-02-05 05:41:15 +0000

    routes

--- cds_demo/config/routes.rb
+++ cds_demo/config/routes.rb
@@ -32,4 +32,5 @@ ActionController::Routing::Routes.draw do |map|
   # Install the default routes as the lowest priority.
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
+  map.connect '', :controller => "/calendar", :action => "redirect"
 end
